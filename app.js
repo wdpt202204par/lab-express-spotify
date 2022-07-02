1dee230e142f732d582396fa8d4759914d72632d
unknown <Jossuha@hotmail.fr>
2022-07-02 11:55:52 +0200

    Iteration 4 OK

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
 // setting the spotify-api goes here:
 
 // Our routes go here:
@@ -34,16 +35,34 @@ app.get("/artist-search", (req, res, next) => {
   spotifyApi
     .searchArtists(req.query.artist)
     .then(data => {
-      console.log('The received data from the API: ', data.body);
+      //console.log('The received data from the API: ', data.body);
 
       res.render("search-results", {
         title: `Result for ${req.query.artist}`,
         data: data.body.artists.items
       })
-      console.log('The received data from the API ITEMS: ', data.body.artists.items);
+      //console.log('The received data from the API ITEMS: ', data.body.artists.items);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
     })
     .catch(err => console.log('The error while searching artists occurred: ', err));
 })
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+  .then(
+    function(data) {
+      console.log('Artist albums', data.body);
+
+      res.render("artist-search-results", {
+        title: `Result for ${data.body.items[0].artists[0].name}`,
+        data: data.body.items
+      })
+    })
+  .catch(
+    function(err) {
+      console.error(err);
+    }
+  );
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
